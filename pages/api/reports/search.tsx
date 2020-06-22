a60f4f6a0f2f7f0ab09149456757a7855a8789f5
Jacob Lurye <jlurye@jimmy.harvard.edu>
2020-06-22 09:23:48 -0400

    Add console logs in search function

--- pages/api/reports/search.tsx
+++ pages/api/reports/search.tsx
@@ -22,12 +22,17 @@ export default async (
 
   // Build the search index if it doesn't exist
   if (INDEX === undefined) {
+    console.log("building new flexsearch index", Date.now());
     INDEX = FlexSearch.create(flexSearchConfig);
     const collection = await getFieldContactCollection();
     // @ts-ignore
     INDEX.add(Object.values(collection));
+    console.log("build complete", Date.now());
   }
 
+  console.log("starting search");
+  console.log(INDEX.info());
   const result = await INDEX.search({ query: q as string, limit: 25 });
+  console.log("search complete");
   return res.json(result);
 };
