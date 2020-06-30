c9dd21179851aaaca6b612ccb7d7b381f31c3049
Jacob Lurye <jlurye@jimmy.harvard.edu>
2020-06-29 23:01:37 -0400

    Revert "Make summary text h6"
    This reverts commit bbf06153b5c884981559b77333e24273322c4178.

--- components/SearchResultsSummary.tsx
+++ components/SearchResultsSummary.tsx
@@ -52,16 +52,20 @@ const SearchResultsSummary: React.FC<SearchResultsSummaryProps> = ({
         <Grid item xs={12} sm={12} md={6}>
           <Grid container direction="column" spacing={1}>
             <Grid item>
-              <Typography variant="h6">
+              <Typography>
                 Found {summary.total} field contact
                 {summary.total !== 1 ? "s" : ""} with "{searchTerm}" in the
                 description.
               </Typography>
             </Grid>
             <Grid item>
-              <Typography variant="h6">
-                {friskPercent} involved a frisk search.
-              </Typography>
+              <Typography>{friskPercent} involved a frisk search.</Typography>
+            </Grid>
+            <Grid item>
+              <StatsGroup title="basis" data={basisPercents} />
+            </Grid>
+            <Grid item>
+              <Divider />
             </Grid>
             <Grid item>
               <StatsGroup title="race" data={racePercents} />
@@ -71,12 +75,6 @@ const SearchResultsSummary: React.FC<SearchResultsSummaryProps> = ({
             </Grid>
             <Grid item>
               <StatsGroup title="gender" data={genderPercents} />
-            </Grid>
-            <Grid item>
-              <Divider />
-            </Grid>
-            <Grid item>
-              <StatsGroup title="basis" data={basisPercents} />
             </Grid>
             <Hidden mdUp>
               <Grid item>
