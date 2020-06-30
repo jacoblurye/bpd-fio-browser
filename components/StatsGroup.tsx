9445139e9da55e63ab19aefc416b32d95deb774f
Jacob Lurye <jlurye@jimmy.harvard.edu>
2020-06-29 22:53:27 -0400

    Chip-ify summary data

--- components/StatsGroup.tsx
+++ components/StatsGroup.tsx
@@ -1,18 +1,11 @@
 import React from "react";
 import { Dictionary, map } from "lodash";
-import { Grid, Typography, makeStyles } from "@material-ui/core";
+import { Grid, Typography, makeStyles, Chip } from "@material-ui/core";
 import theme from "style/theme";
 
 const useStyles = makeStyles({
   statValue: {
     fontWeight: theme.typography.fontWeightBold,
-  },
-  statLabel: {
-    lineHeight: 1,
-  },
-  statLabelContainer: {
-    textAlign: "center",
-    overflowWrap: "normal",
   },
 });
 
@@ -28,22 +21,23 @@ const StatsItem: React.FC<{
   const classes = useStyles();
 
   return (
-    <Grid container direction="column" justify="center">
-      <Grid item>
-        <Typography className={classes.statValue} align="center">
-          {value}
-        </Typography>
-      </Grid>
-      <Grid item className={classes.statLabelContainer}>
-        <Typography
-          className={classes.statLabel}
-          align="center"
-          variant="overline"
-        >
-          {label}
-        </Typography>
-      </Grid>
-    </Grid>
+    <Chip
+      size="small"
+      label={
+        <Grid container wrap="nowrap" spacing={1} alignItems="center">
+          <Grid item>
+            <Typography align="center" variant="overline">
+              {label}
+            </Typography>
+          </Grid>
+          <Grid item>
+            <Typography className={classes.statValue} variant="body2">
+              {value}
+            </Typography>
+          </Grid>
+        </Grid>
+      }
+    />
   );
 };
 
@@ -51,14 +45,12 @@ const StatsGroup: React.FC<StatsGroupProps> = ({ title, data }) => {
   return (
     <Grid container direction="column">
       <Grid item>
-        <Typography color="textSecondary" variant="overline">
-          {title}
-        </Typography>
+        <Typography variant="overline">{title}</Typography>
       </Grid>
       <Grid item>
-        <Grid container justify="space-evenly" spacing={1}>
+        <Grid container spacing={1}>
           {map(data, (value, label) => (
-            <Grid item key={label} xs={6} sm={3}>
+            <Grid item key={label}>
               <StatsItem label={label} value={value} />
             </Grid>
           ))}
