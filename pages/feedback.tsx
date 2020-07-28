93d009369dad3f300f277041a043fe1922746566
Jacob Lurye <jlurye@jimmy.harvard.edu>
2020-07-27 21:53:35 -0400

    Add embedded google form to feedback page

--- pages/feedback.tsx
+++ pages/feedback.tsx
@@ -1,10 +1,23 @@
 import Layout from "components/Layout";
-import { Typography } from "@material-ui/core";
+import { Box } from "@material-ui/core";
 
-const FeedbackPage = () => (
-  <Layout title="Feedback">
-    <Typography>Coming soon!</Typography>
-  </Layout>
-);
+const FeedbackPage = () => {
+  return (
+    <Layout title="Feedback">
+      <Box textAlign="center">
+        <iframe
+          src="https://docs.google.com/forms/d/e/1FAIpQLSfPBWGFsG2VZdL5By-AmOE17FCZuMa0zXlbk43hw3KKm4g1jA/viewform?embedded=true"
+          height={1000}
+          width="100%"
+          frameBorder={0}
+          marginHeight={0}
+          marginWidth={0}
+        >
+          Loading…
+        </iframe>
+      </Box>
+    </Layout>
+  );
+};
 
 export default FeedbackPage;
