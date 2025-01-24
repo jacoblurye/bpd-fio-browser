02b4267126708526c39165ada0e2bcb564baaf63
Jacob Lurye <lurye.jacob@gmail.com>
2025-01-23 20:31:53 -0500

    package version bumps

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,7 +1,5 @@
 /// <reference types="next" />
-/// <reference types="next/types/global" />
+/// <reference types="next/image-types/global" />
 
-declare module "*.fsindex" {
-  const content: string;
-  export default content;
-}
+// NOTE: This file should not be edited
+// see https://nextjs.org/docs/pages/api-reference/config/typescript for more information.
